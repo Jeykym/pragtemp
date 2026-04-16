4475026ac4b9937f58086ab97438f977fc91582b
Jáchym Šulman <jachym.sulman@gmail.com>
2026-04-16 12:57:13 +0200

    add test golemio api request

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+import requests
+from dotenv import load_dotenv
+import os
+from urllib.parse import urljoin
+import json
 
-app = FastAPI()
+BASE_URL = 'https://api.golemio.cz'
+ENDPOINT = '/v2/microclimate/locations'
+API_KEY_NAME = 'API_KEY'
+API_KEY_HEADER_NAME = 'X-Access-Token'
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello world'}
+load_dotenv()
+
+API_KEY = os.getenv(API_KEY_NAME)
+if not API_KEY:
+    raise ValueError(f'API key named "{API_KEY_NAME}" is not set in .env')
+
+response = requests.get(
+    urljoin(BASE_URL, ENDPOINT),
+    headers={API_KEY_HEADER_NAME: API_KEY}
+)
+print(json.dumps(response.json(), indent=2))
